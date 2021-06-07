41d0bb4cf1d0438218823abcb3150d30e49871fc
WWHash <Reshetnikov.Hash@yandex.ru>
2021-06-07 22:00:02 +0300

    За адаптивил шапку, меню, подвал

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,37 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var nav = document.querySelector('.nav');
+var navToggle = document.querySelector('.nav__burger');
 
-pageHeader.classList.remove('page-header--nojs');
+nav.classList.remove('nav--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+navToggle.addEventListener('click', function () {
+  if (nav.classList.contains('nav--closed')) {
+    nav.classList.remove('nav--closed');
+    nav.classList.add('nav--opened');
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    nav.classList.add('nav--closed');
+    nav.classList.remove('nav--opened');
+  }
+});
+
+window.addEventListener('resize', function () {
+  var TABLET_MEDIA_QUERY = '(max-width: 1023px)';
+  var navList = document.querySelector('.nav__menu-list');
+  var navListHidden = document.querySelector('.nav__menu-list--hidden');
+  var navMenu = document.querySelector('.nav__menu');
+  var top = document.querySelector('.nav__top');
+  var logo = top.querySelector('.nav__logo');
+  var initList = top.querySelector('.nav__init-list');
+  var bas = document.querySelector('.nav__init-item--bas');
+  var search = document.querySelector('.nav__search');
+  var login = document.querySelector('.nav__init-item--login');
+
+  if (window.matchMedia(TABLET_MEDIA_QUERY).matches) {
+    navMenu.insertBefore(search, navList);
+    navMenu.insertBefore(login, navListHidden);
+  }
+  else {
+    top.insertBefore(search, logo);
+    initList.insertBefore(login, bas);
   }
 });
